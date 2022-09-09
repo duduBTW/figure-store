9bc81388692939b7b57a4b8e4e873511d5053387
dudubtw <carloseduardo108090@hotmail.com>
2022-09-09 09:55:44 -0300

    derp

--- src/server/client/services.ts
+++ src/server/client/services.ts
@@ -1,14 +1,14 @@
 import axios from "axios";
+
 import * as figureServices from "./figures";
 import * as cartServices from "./cart";
 import * as orderServices from "./order";
 import * as imagesServices from "./images";
 import * as adressServices from "./adress";
 import * as userServices from "./user";
-import { env } from "constants/client.mjs";
 
 export const api = axios.create({
-  baseURL: env.API_BASE_URL,
+  baseURL: process.env.API_BASE_URL,
 });
 
 const service = {
